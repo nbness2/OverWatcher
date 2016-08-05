f990d3985751653efaab694b7456415c4a522109
nbness2 <nbness1337@gmail.com>
2016-08-05 00:21:32 -0600

    added newlines before every print
    just makes it a little easier to see

--- tests.py
+++ tests.py
@@ -16,20 +16,20 @@ def wait():
 def tag_test(tag, platform, region):
     #runs basic tests to fetch info.
     print('testing with tag {}, platform {}, in region {}'.format(tag, platform, region))
-    print('getting basic profile info (level, time played, competetive rank, etc.)')
+    print('\ngetting basic profile info (level, time played, competetive rank, etc.)')
     print(w.get_profile(tag, platform, region))
-    print('getting achievements')
+    print('\ngetting achievements')
     print(w.get_achievements(tag, platform, region))
-    print('getting general gameplay stats (total damage done, total healing done, cards, etc.)')
+    print('\ngetting general gameplay stats (total damage done, total healing done, cards, etc.)')
     print(w.get_general_stats(tag, platform, region))
-    print('getting stats for all heroes in quick play')
+    print('\ngetting stats for all heroes in quick play')
     heroes = [hero for hero in overwatcher.heroes]
     print(w.get_heroes_stats(tag, heroes, platform, region))
-    print('getting playtime for all heroes in quick play')
+    print('\ngetting playtime for all heroes in quick play')
     print(w.get_hero_playtime(tag, platform, region))
-    print('getting stats for all heroes in comptitive play')
+    print('\ngetting stats for all heroes in comptitive play')
     print(w.get_heroes_stats(tag, heroes, platform, region, mode='competitive-play'))
-    print('getting playtime for all heroes in competitive play')
+    print('\ngetting playtime for all heroes in competitive play')
     print(w.get_hero_playtime(tag, platform, region, mode='competitive-play'))
 
 
